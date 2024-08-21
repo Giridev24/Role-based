21ea4b7a04c66ee43e50bc66238e4165c5811e8d
Giridev24 <140387591+Giridev24@users.noreply.github.com>
2024-08-21 09:38:10 +0530

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -76,7 +76,7 @@ function Login() {
 
   return (
     <div className="f">
-       <h4 className="text-white text-center mb-auto ml-auto"># Role-Based Access Control</h4>
+       <h4 className="text-white text-center mb-auto mx-auto"># Role-Based Access Control</h4>
       <div className="ids p-md-5" data-aos="fade-in">
      
         <h4 className="text-white">Clients</h4>
